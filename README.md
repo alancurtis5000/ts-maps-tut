57a7bc0f9a6ccb3668d223b082da09696cbc1f43
Alan <alancurtis5000+accounts@gmail.com>
2022-05-06 12:01:01 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Maps Project typescript tut
+
+install depencies
+`npm i -g parcel-bundler`
+`npm i`
+
+to run app;
+`parcel index.html` from project root dir
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { User } from "./models/User";
+import { Company } from "./models/Company";
+import { Map } from "./models/Map";
+
+const user = new User();
+const company = new Company();
+const map = new Map("map");
+
+map.addMarker(user);
+map.addMarker(company);
+
+console.log("Hello", user, company);
